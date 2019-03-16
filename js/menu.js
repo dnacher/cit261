fb2e2f2b37b1792ab22b54c8e0c3bb6573237f04
DNacher <danielnacher@gmail.com>
2019-03-16 13:59:16 -0300

    add loops

--- js/menu.js
+++ js/menu.js
@@ -40,3 +40,26 @@ function hello(){
 	ctx.font = "30px Arial";
 	ctx.fillText("Hello CIT-261", 10, 50);
 }
+
+function createLoop() {
+	var value = document.getElementById("value").value;
+  if(!isNaN(value)){
+	  var text = "";
+
+	  for(var i=0; i<value;i++){  					
+		  text+= "this is a loop with the numbers you entered N°: " + (i+1) + "<br>";
+	  }
+	  document.getElementById("demo").innerHTML = text;
+  }else{
+	  document.getElementById("demo").innerHTML = "please type a number in this option";
+  }
+}
+
+function loop10Times(){
+	var value = document.getElementById("value").value;
+	var text = "";
+	for(var i=0; i<7;i++){  					
+		text+= value + ": #" + (i+1) +  "<br>";
+	}
+	document.getElementById("demo").innerHTML = text;
+}
